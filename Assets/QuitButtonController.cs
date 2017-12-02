f4001259771e2a4babf599cc63e1a8527fe3afd6
RNPGhost <samuelfhutton@gmail.com>
2017-12-02 10:44:40 +0000

    add quit button to main menu

---
+++ Assets/QuitButtonController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButtonController : MonoBehaviour {
+  // set in editor
+  public GameObject _mainCanvas;
+  public GameObject _confirmQuitCanvas;
+  
+  // interface
+  public void Quit() {
+    _mainCanvas.SetActive(false);
+    _confirmQuitCanvas.SetActive(true);
+  }
+
+  public void ConfirmQuit() {
+    Application.Quit();
+  }
+
+  public void CancelQuit() {
+    _confirmQuitCanvas.SetActive(false);
+    _mainCanvas.SetActive(true);
+  }
+}
